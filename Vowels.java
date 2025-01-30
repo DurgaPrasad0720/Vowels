28258810d9ba31e7d7fff0dc367737590401622e
DurgaPrasad0720 <mmprasad901@gmail.com>
2025-01-30 20:35:02 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Vowels {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);{
+            System.out.println("Enter a String: ");
+            String string=scanner.nextLine();
+            int vowelcount=0;
+            for (char n : string.toCharArray()){
+                if("aieou".indexOf(n) != -1){
+                    vowelcount++;
+                }
+            }
+            System.out.println("Number of vowels in the given string: "+vowelcount);
+            scanner.close();
+        }
+    }
+}
